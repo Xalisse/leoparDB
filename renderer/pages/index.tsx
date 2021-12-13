7bb944a8d1376862d9402401a425dadac6da033e
Valentine Lejeune <valentine.lejeune-ext@sanofi.com>
2021-12-13 16:29:25 +0100

    feat: add typeorm

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,15 +1,36 @@
 import Layout from '../components/Layout'
+import {createConnection} from "typeorm";
 
-const IndexPage = () => {
-  
-
+const IndexPage = ({ tableNames, dbNames }) => {
   return (
     <Layout title="LeoparDB 🐆">
       <p>
         The easiest way to visualize your databases
       </p>
+      {tableNames.map((table, index) => (
+       <span className='indent' key={index} > {table.table_name} </span>
+      ))}
+      {dbNames.map((db, index) => (
+       <span className='indent' key={index + 'db'} > {db.Database} </span>
+      ))}
     </Layout>
   )
 }
+
+// This gets called on every request
+export async function getServerSideProps() {
+    const db = await createConnection({
+      type: "mysql",
+      host: "localhost",
+      port: 8889,
+      username: "root",
+      password: "root",
+      logging: false,
+    });
+    const tableNames = await db.query('SELECT table_name FROM information_schema.tables WHERE table_schema = "M3104"');
+    const dbNames = await db.query('SHOW DATABASES');
+  return { props: { tableNames, dbNames } }
+}
+
 
 export default IndexPage
