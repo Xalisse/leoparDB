3f4f3fcd4029312b9a576e0918630a1ba94a87d4
Xalis <valentine.lejeune1@gmail.com>
2022-03-17 17:11:34 +0100

    feat: refactor navigation

--- renderer/components/navigation.tsx
+++ renderer/components/navigation.tsx
@@ -1,17 +1,57 @@
+import { Disclosure } from "@headlessui/react";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { Database } from "../interfaces";
 
-type Props = {
-  databases: Database[];
-};
 
-const Navigation = ({ databases }: Props) => {
+const Navigation = () => {
+  const [databases, setDatabases] = useState<Database[]>([]);
+  useEffect(() => {
+    async function fetchDatabases() {
+      const response = await axios.post(
+        "/api/databases",
+        {
+          type: "mysql",
+          host: "localhost",
+          port: 3306,
+          username: "root",
+          password: "root",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status !== 200) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      console.log(response.data)
+      setDatabases(response.data);
+    }
+
+    fetchDatabases();
+  }, []);
+
   return (
-    <div className="bg-gray-100 h-screen sticky top-0 p-1 pr-3">
+    <div className="bg-gray-100 h-full sticky top-0 p-1 pr-3 w-[200px] overflow-hidden">
       {databases.map((database) => (
-        <Link href={`/database/${database.name}`} key={database.name}>
+      <Disclosure>
+        <Disclosure.Button className="py-2 truncate">
           <div className="cursor-pointer">{database.name}</div>
-        </Link>
+        </Disclosure.Button>
+          <Disclosure.Panel>
+            {database.tables.map((table) => (
+              <div className="ml-4 truncate">
+                <Link href={`/database/${database.name}/${table}`} key={table}>
+                  {table}
+                </Link>
+              </div>
+            ))}
+        </Disclosure.Panel>
+      </Disclosure>
       ))}
     </div>
   );
